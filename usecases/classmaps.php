b2a3d839c71b1018df1b8e3573f2fb810e4b59d9
Manuel Lemos <mlemos@acm.org>
2023-05-12 19:01:28 -0300

    Added the oauth2_server_login_dialog_class class.

--- usecases/classmaps.php
+++ usecases/classmaps.php
@@ -12,6 +12,7 @@
 	'oauth2_server_api_example_user_class'=>__DIR__.DIRECTORY_SEPARATOR.'example'.DIRECTORY_SEPARATOR.'auth2_server_api_example_user.php',
 	'oauth2_server_error_class'=>__DIR__.DIRECTORY_SEPARATOR.'oauth2_server_error.php',
 	'oauth2_server_handler_default_class'=>__DIR__.DIRECTORY_SEPARATOR.'oauth2_server_handler_default.php',
+	'oauth2_server_login_dialog_class'=>__DIR__.DIRECTORY_SEPARATOR.'oauth2_server_login_dialog.php',
 	'oauth2_server_maintenance_class'=>__DIR__.DIRECTORY_SEPARATOR.'oauth2_server_maintenance.php',
 	'oauth2_server_token_class'=>__DIR__.DIRECTORY_SEPARATOR.'oauth2_server_token.php',
 );
